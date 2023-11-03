3de093758752cfcecc434f11036da897c67a67e8
Sergey Maximenko <maks.sergey.11@gmail.com>
2023-11-03 19:53:08 +0300

    calc

--- js/script.js
+++ js/script.js
@@ -461,42 +461,79 @@ window.addEventListener('DOMContentLoaded', () => {
                         });
                 });
 
-/*                 showSlides(slideIndex);
 
-                if (slides.length < 10) {
-                        total.textContent = `0${slides.length}`;
-                } else {
-                        total.textContent = slides.length;
-                }
 
-                function showSlides(n) {
 
-                        if (n >slides.length) {
-                                slideIndex = 1;
+                //Calc
+
+
+                const result = document.querySelector('.calculating__result span');
+                let sex = 'female', 
+                height, weight, age, 
+                ratio = 1.375;
+
+                function calcTotal() {
+                        if (!sex || !height || !weight || !age || !ratio) {
+                                result.textContent = '____';
+                                return;
                         }
-                        if (n < 1) {
-                                slideIndex = slides.length;
-                        }
 
-                        slides.forEach(item => item.style.display = 'none');
-                        slides[slideIndex - 1].style.display = 'block';
-
-                        if (slides.length < 10) {
-                                current.textContent = `0${slideIndex}`;
+                        if (sex == 'female') {
+                                result.textContent = Math.round((447.6 + (9.2 * weight) + (3.1 * height) - (4.3 * age)) * ratio);
                         } else {
-                                current.textContent = slideIndex;
+                                result.textContent =  Math.round((88.36 + (13.4 * weight) + (4.8 * height) - (5.7 * age)) * ratio);
                         }
                 }
 
-                function plusSlides(n) {
-                        showSlides(slideIndex += n);
+                calcTotal();
+
+                function getStaticInformation(parentSelector, activeClass) {
+                        const elements = document.querySelectorAll(`${parentSelector} div`);
+
+                        elements.forEach(elem => {
+                                elem.addEventListener('click', (e) => {
+                                        if (e.target.getAttribute('data-ratio')) {
+                                                ratio = +e.target.getAttribute('data-ratio');
+                                        } else {
+                                                sex = e.target.getAttribute('id');
+                                        }
+        
+                                        elements.forEach(elem => {
+                                                elem.classList.remove(activeClass);
+                                        });
+        
+                                        e.target.classList.add(activeClass);
+                                        calcTotal();
+                                });
+                        });
                 }
 
-                prev.addEventListener('click', () => {
-                        plusSlides(-1)
-                });
-                next.addEventListener('click', () => {
-                        plusSlides(1)
-                }); */
+                getStaticInformation('#gender', 'calculating__choose-item_active');
+                getStaticInformation('.calculating__choose_big', 'calculating__choose-item_active');
+
+                function getDynamicInformation(selector) {
+                        const input = document.querySelector(selector);
+
+                        input.addEventListener('input', () => {
+                                switch(input.getAttribute('id')) {
+                                        case 'height': 
+                                                height = +input.value;
+                                                break;
+                                        case 'weight':
+                                                weight = +input.value;
+                                                break;
+                                        case 'age':
+                                                age = +input.value;
+                                                break;
+                                }
+                                calcTotal();
+                        });
+                }
+
+                
+                getDynamicInformation('#height');
+                getDynamicInformation('#weight');
+                getDynamicInformation('#age');
+
 
 });
